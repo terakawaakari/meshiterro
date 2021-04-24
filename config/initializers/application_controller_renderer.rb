43692d27371ae3a223e931809363af4616119071
terakawaakari <terakawaakari0701@gmail.com>
2021-04-24 04:24:10 +0000

    [Finish]4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'c338356e52b369ae91c3b7d39ce625e95367e6156d2769fa0b0448689ab5d45f42f0a55eb0a5ca1392924f315b7122f2805a8238fbe683ff7cc5ad82627e84ec'
