1a5bdc3ce1965afa9bed43f506694a7d1f8d698a
terakawaakari <terakawaakari0701@gmail.com>
2021-05-29 15:07:18 +0000

    Sign upに住所登録フォーム追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,13 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [
+      :name,
+      :postcode,
+      :prefecture_name,
+      :address_city,
+      :address_street,
+      :address_building
+    ])
   end
-
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,15 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
 
   attachment :profile_image
+  
+  include JpPrefecture
+  jp_prefecture :prefecture_code
+  
+  def prefecture_name
+    JpPrefecture::Prefecture.find(code: prefecture_code).try(:name)
+  end
+  
+  def prefecture_name=(prefecture_name)
+    self.prefecture_code = JpPrefecture::Prefecture.find(name: prefecture_name).code
+  end
 end
